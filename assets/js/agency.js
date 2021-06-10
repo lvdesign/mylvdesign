2ce562a368b4add629560f7b4b70ed234118f333
lvdesignfr <work@lvdesign.com.fr>
2021-06-10 17:13:46 +0200

    sw4

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -58,6 +58,15 @@
     }, 300);
   
     // Service worker 
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register(' https://lvdesign.github.io/mylvdesign/basicServiceWorker.js', { scope: ' https://lvdesign.github.io/mylvdesign/' }).then(function(reg) {
+        // registration worked
+        console.log('Registration succeeded. Scope is ' + reg.scope);
+      }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+      });
+    };
     
   
   })(jQuery); // End of use strict
